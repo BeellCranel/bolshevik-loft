82e509adc13274b38e78f2e7191404510a2afbb4
Roma Dergach <dergachroma@gmail.com>
2022-11-01 19:42:48 +0300

    feat: mainPageGallery is done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 // import Messengers from "../../UI/Messengers/Messengers";
 import "./Main.css";
 import MainPageAffiche from "./MainPageAffiche/MainPageAffiche";
+import MainPageGallery from "./MainPageGallery/MainPageGallery";
 import MainPageHeader from "./MainPageHeader/MainPageHeader";
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
     <main>
       <MainPageHeader />
       <MainPageAffiche />
+      <MainPageGallery />
     </main>
   );
 };
